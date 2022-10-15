191e47ab84d283f106d25d866c3d20cd2e351856
BoomMan <l.o.v.e_y.o.u_3344@qq.com>
2022-10-15 15:29:19 +0800

    feat(add nacos-client-demo):

---
+++ nacos-client-demo/src/main/java/com/boommanpro/nacosclientdemo/NacosClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.boommanpro.nacosclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClientDemoApplication.class, args);
+    }
+
+}
